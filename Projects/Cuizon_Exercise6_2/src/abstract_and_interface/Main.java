6de56616ebbcb0801ab3c15733186116d8309509
shanejitsu <shane.cuizon@awsys-i.com>
2024-11-05 16:01:01 +0800

    Modify Main class

--- Projects/Cuizon_Exercise6_2/src/abstract_and_interface/Main.java
+++ Projects/Cuizon_Exercise6_2/src/abstract_and_interface/Main.java
@@ -3,7 +3,7 @@ package abstract_and_interface;
 public class Main {
 
 	public static void main(String[] args) {
-		Hangman h = new Hangman("Shane");
+		Hangman h = new Hangman("banana");
 		h.start();
 
 	}
