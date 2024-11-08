95f26dd1910afcd5f0eb74c164eac869b21a1c7e
shanejitsu <shane.cuizon@awsys-i.com>
2024-11-08 16:46:01 +0800

    Modify Main

--- Projects/Cuizon_Exercise6_2/src/abstract_and_interface/Main.java
+++ Projects/Cuizon_Exercise6_2/src/abstract_and_interface/Main.java
@@ -3,7 +3,7 @@ package abstract_and_interface;
 public class Main {
 
 	public static void main(String[] args) {
-		Hangman h = new Hangman("banana");
+		Hangman h = new Hangman("NEVER GONNA GIVE YOU UP");
 		h.start();
 
 	}
