02183911c97a25d56d1985546d531c06c8e819c3
shanejitsu <shane.cuizon@awsys-i.com>
2024-11-11 14:16:36 +0800

    Modify Exercise 8

--- Projects/Cuizon_Exercise8/src/java_collections/Main.java
+++ Projects/Cuizon_Exercise8/src/java_collections/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		WeatherRecorder rec = new WeatherRecorder();
 		rec.start();
-		rec.displayWeatherTemp("Monday");
+		rec.getWeatherTemp();	
 	}
-
 }
--- Projects/Cuizon_Exercise8/src/java_collections/WeatherRecorder.java
+++ Projects/Cuizon_Exercise8/src/java_collections/WeatherRecorder.java
@@ -8,13 +8,28 @@ public class WeatherRecorder {
 	static Scanner scn = new Scanner(System.in);
 	HashMap<String, HashMap<String, Double>> weatherMap;
 
+	/**
+	 * This the ENUM class the holds the valid weather names
+	 */
+	public enum Weather {
+		RAINY, SUNNY, WINDY, STORMY, CLOUDY, SNOWY;
+	}
+
+	/**
+	 * This the ENUM class the holds the valid day names
+	 */
+	public enum Days {
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+	}
+
 	public static class InvalidWeatherException extends Exception {
 		private static final long serialVersionUID = 1L;
 
 		/**
 		 * This function throws an exception if the parameter is invalid
+		 * 
 		 * @param input holds the value String
-		 * */
+		 */
 		public static void validateWeather(String input) throws InvalidWeatherException {
 			boolean isValid = false;
 			for (Weather weather : Weather.values()) {
@@ -30,23 +45,48 @@ public class WeatherRecorder {
 
 	}
 
-	public enum Weather {
-		RAINY, SUNNY, WINDY, STORMY, CLOUDY, SNOWY
-	}
-
-	public enum Days {
-		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
-	}
-
+	/**
+	 * This is the WeatherRecorder class constructor. This initializes the HashMap
+	 * weatherMap.
+	 */
 	public WeatherRecorder() {
 		weatherMap = new HashMap<>();
+		HashMap<String, Double> sundayWeatherTemp = new HashMap<>();
+		sundayWeatherTemp.put(Weather.RAINY.name(), 25.6);
+		weatherMap.put("SUNDAY", sundayWeatherTemp);
+
+		HashMap<String, Double> mondayWeatherTemp = new HashMap<>();
+		mondayWeatherTemp.put(Weather.SUNNY.name(), 28.8);
+		weatherMap.put("MONDAY", mondayWeatherTemp);
+
+		HashMap<String, Double> tuesdayWeatherTemp = new HashMap<>();
+		tuesdayWeatherTemp.put(Weather.SNOWY.name(), -11.2);
+		weatherMap.put("TUESDAY", tuesdayWeatherTemp);
+
+		HashMap<String, Double> wednesdayWeatherTemp = new HashMap<>();
+		wednesdayWeatherTemp.put(Weather.STORMY.name(), 25.1);
+		weatherMap.put("WEDNESDAY", wednesdayWeatherTemp);
+
+		HashMap<String, Double> thursdayWeatherTemp = new HashMap<>();
+		thursdayWeatherTemp.put(Weather.CLOUDY.name(), 26.4);
+		weatherMap.put("THURSDAY", thursdayWeatherTemp);
+
+		HashMap<String, Double> fridayWeatherTemp = new HashMap<>();
+		fridayWeatherTemp.put(Weather.SUNNY.name(), 30.1);
+		weatherMap.put("FRIDAY", fridayWeatherTemp);
+
+		HashMap<String, Double> saturdayWeatherTemp = new HashMap<>();
+		saturdayWeatherTemp.put(Weather.SNOWY.name(), -10.11);
+		weatherMap.put("SATURDAY", saturdayWeatherTemp);
+
 	}
+
 	/**
-	 * Prompts the user to input a VALID weather and temperature of a day
-	 * puts the input values into the weatherMap HashMap
+	 * Prompts the user to input a VALID weather and temperature of a day puts the
+	 * input values into the weatherMap HashMap
 	 * 
 	 * @return void
-	 * */
+	 */
 	public void start() {
 		Days[] days = Days.values();
 		for (int i = 0; i < days.length;) {
@@ -65,6 +105,7 @@ public class WeatherRecorder {
 						isValid = true;
 						scn.nextLine();
 						HashMap<String, Double> weatherTemp = new HashMap<>();
+						;
 						weatherTemp.put(weather, temperature);
 						weatherMap.put(days[i].name(), weatherTemp);
 					} catch (InputMismatchException e) {
@@ -73,16 +114,16 @@ public class WeatherRecorder {
 						scn.nextLine();
 					}
 				}
-				i++;//only move to the next day once the former day is inputed with valid values
+				i++;// only move to the next day once the former day is inputed with valid values
 
 			} catch (InvalidWeatherException e) {
-				System.out.println("Please make sure to input a valid weather.");
+				System.out.println("Please make sure to input a valid weather name.");
 				for (int j = 0; j < Weather.values().length; j++) {
 					System.out.print(Weather.values()[j]);
 					if (j < Weather.values().length - 1) {
 						System.out.print(", ");
 					} else {
-						System.out.println();
+						System.out.println("\n");
 					}
 				}
 			}
@@ -98,14 +139,12 @@ public class WeatherRecorder {
 	public void displayWeatherTemp(String day) {
 		try {
 			Days dayEnum = Days.valueOf(day.toUpperCase());
-			System.out.println("Valid day: " + dayEnum);
-
-			HashMap<String, Double> weatherTemp = weatherMap.get(dayEnum.name());
+			HashMap<String, Double> weatherTemp = weatherMap.get(day.toUpperCase());
 			if (weatherTemp != null) {
 				for (String weather : weatherTemp.keySet()) {
 					Double temperature = weatherTemp.get(weather);
-					System.out.println("The weather on " + dayEnum + " was " + weather + " with a temperature of "
-							+ temperature + "°C.");
+					System.out.println("The weather on " + dayEnum + " was " + weather.toLowerCase()
+							+ " with a temperature of " + temperature + "°C.");
 				}
 			} else {
 				System.out.println("No weather data available for " + dayEnum);
@@ -114,6 +153,11 @@ public class WeatherRecorder {
 		} catch (IllegalArgumentException e) {
 			System.out.println("Invalid day: " + day);
 		}
+	}
+	public void getWeatherTemp() {
+		System.out.print("Enter a day to check its weather and temperture: ");
+		displayWeatherTemp(scn.nextLine());
+		
 	}
 
 }
